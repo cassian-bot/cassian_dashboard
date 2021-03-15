a3b89f83a24018899fa48170af00e48ec10ffdc2
zastrixarundell <zastrixarundell@gmail.com>
2021-03-15 22:35:02 +0100

    Added minimalistic commands page

--- lib/cassian_dashboard_web/controllers/commands_controller.ex
+++ lib/cassian_dashboard_web/controllers/commands_controller.ex
@@ -2,6 +2,19 @@ defmodule CassianDashboardWeb.CommandsController do
   use CassianDashboardWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    commands = [
+      {"ping", nil},
+      {"backward", nil},
+      {"forward", nil},
+      {"next", nil},
+      {"play", "song url"},
+      {"playlist", nil},
+      {"previous", nil},
+      {"repeat", "one, none, all"},
+      {"shuffle", nil},
+      {"unshuffle", nil}
+    ]
+
+    render(conn, "index.html", commands: commands)
   end
 end
