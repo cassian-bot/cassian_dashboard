5df6eb1c247307c23c67d320efda4e8743ae8837
zastrixarundell <zastrixarundell@gmail.com>
2021-03-17 00:13:07 +0100

    Removed error data

--- lib/cassian_dashboard_web/views/error_view.ex
+++ lib/cassian_dashboard_web/views/error_view.ex
@@ -13,8 +13,4 @@ defmodule CassianDashboardWeb.ErrorView do
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
   end
-
-  def error_type(data) do
-    IO.inspect(data)
-  end
 end
