2f16a0ca553dcf5b742087f9bd4735ddf66e5862
zastrixarundell <zastrixarundell@gmail.com>
2021-03-18 21:34:11 +0100

    Starting spotify service

---
+++ lib/cassian_dashboard/services/spotify_service.ex
@@ -0,0 +1,13 @@
+defmodule CassianDashboard.Services.SpotifyService do
+  @moduledoc """
+  Module for the Spotify service. Get account data and
+  update the token.
+  """
+
+  alias CassianDashboard.Connections.Connection
+
+  @spec reauthorize(connection :: %Connection{}) :: any()
+  def reauthorize(connection) do
+
+  end
+end
