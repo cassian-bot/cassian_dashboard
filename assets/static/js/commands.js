5e0c32bb529313a6cf89b0dd3f972202f7aebd13
zastrixarundell <zastrixarundell@gmail.com>
2021-05-08 18:39:11 +0200

    Added hash redirect

--- assets/static/js/commands.js
+++ assets/static/js/commands.js
@@ -4,4 +4,7 @@ if(spotifyButton.classList.contains("not-connected"))
     spotifyButton.parentElement.href = '/auth/spotify'
 else {
     spotifyButton.parentElement.href = 'javascript:void(0)'
+    spotifyButton.onclick = () => {
+        window.location.href = '/commands#spotify'
+    }
 }
