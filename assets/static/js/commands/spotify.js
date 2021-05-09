c711ca8503fed5bf723207066a45b516232cd17b
zastrixarundell <zastrixarundell@gmail.com>
2021-05-09 02:19:35 +0200

    Added logic for showing playlist and inputing field

--- assets/static/js/commands/spotify.js
+++ assets/static/js/commands/spotify.js
@@ -1,8 +1,22 @@
 fetch(`/api/v1/spotify/${spotify_id}/index_playlists`)
     .then(response => response.json())
     .then(data => {
+        if(data.length <= 0) return;
+
+        holder = document.getElementById("playlists");
+
+        holder.classList.remove('hidden');
+
         for(let i=0; i < data.length; i++) {
             playlist = data[i];
-            console.log(playlist);
+
+            const new_element = document.createElement('p');
+            new_element.classList.add("rounded-box", "category-element", "command-category");
+            new_element.textContent = data[i].name;
+            new_element.onclick = () => {
+                window.location.href = `/commands?provider=spotify&placeholder=${data[i].link}`
+            }
+            
+            holder.appendChild(new_element);
         }
     })
