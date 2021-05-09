e4fc074db6313a10954e6428b3a01e389e75861c
zastrixarundell <zastrixarundell@gmail.com>
2021-05-09 12:50:43 +0200

    Ran formatter

--- lib/cassian_dashboard_web/controllers/commands_controller.ex
+++ lib/cassian_dashboard_web/controllers/commands_controller.ex
@@ -6,8 +6,10 @@ defmodule CassianDashboardWeb.CommandsController do
   defguardp real_provider(provider) when provider in ["spotify", "general", "youtube"]
 
   def index(conn, %{"provider" => provider}) when real_provider(provider) do
-
-    render(conn, "index.html", provider: provider, playlists: get_playlist(conn, provider |> String.to_atom()))
+    render(conn, "index.html",
+      provider: provider,
+      playlists: get_playlist(conn, provider |> String.to_atom())
+    )
   end
 
   def index(conn, _) do
@@ -17,17 +19,21 @@ defmodule CassianDashboardWeb.CommandsController do
   @doc """
   Get playlists for specific connections.
   """
-  @spec get_playlist(conn :: %Plug.Conn{}, provider :: :spotify | any()) :: [%{name: String.t(), link: String.t()}]
+  @spec get_playlist(conn :: %Plug.Conn{}, provider :: :spotify | any()) :: [
+          %{name: String.t(), link: String.t()}
+        ]
   def get_playlist(conn, :spotify) do
     user = current_user(conn)
 
     case Connections.connection_for_account(user, "spotify") do
       nil ->
         []
+
       connection ->
         case SpotifyService.get_playlists(connection) do
           {:ok, playlists} ->
             Enum.map(playlists, fn p -> %{name: p.name, link: p.external_urls.spotify} end)
+
           _ ->
             []
         end
